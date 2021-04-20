b2092a3e277d3e2b970cace5adb51ce425682e39
anastasiy-kozlan <79521120+anastasiy-kozlan@users.noreply.github.com>
2021-04-20 22:15:55 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        // Завдання 1
+        /// <summary>
+        /// Sk (finding the number of full weeks)
+        /// </summary>
+        /// <param name="R">the number of days that have elapsed since</param>
+        /// <returns>returns the value of result</returns>
+        
+        public static double Sk(int R)
+        {
+            double result = R/7;
+            return Convert.ToInt32(result);
+        }
+
+
+        // Завдання 2
+        /// <summary>
+        /// S (sum)
+        /// </summary>
+        /// <param name="n">third argument of the S</param>
+        /// <returns>returns the value of result</returns>
+        
+        public static double S(int n)
+        {
+            double result = 0;
+            for (int i=0; i<n; i++)
+            {
+                result = result + Math.Pow((-1), i) * (1 / Math.Pow(2, i));         
+            }
+            return result;
+        }
+
+
+        // Завдання 3
+        /// <summary>
+        /// st (sum)
+        /// </summary>
+        /// <param name="eps">first argument of the st</param>
+        /// <param name="n">second argument of the st</param>
+        /// <param name="z">third argument of the st</param>
+        /// <returns>returns the value of the st</returns>
+        
+        public static double st(double eps, int n, double z)
+        {
+            double st = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                st = st + (Math.Pow((-1), n)) / (fact(n) * z);
+                if (Math.Abs(st) < eps)
+                {
+                    break;
+                }
+            }
+            return st;
+        }
+
+        /// <summary>
+        ///  F
+        /// </summary>
+        /// <param name="eps">first argument of the F</param>
+        /// <param name="n">second argument of the F</param>
+        /// <param name="z">third argument of the F</param>
+        /// <returns>returns the value of the  F</returns>
+
+        public static double F(double eps, int n, double z)
+        {
+            double F = 1 - z * Math.Log(2);
+            return F;
+        }
+
+        /// <summary>
+        /// T
+        /// </summary>
+        /// <param name="n">third argument of the fact</param>
+        /// <returns>returns the value of the  tmp</returns>
+
+        static double fact(int n)
+        {
+            double tmp = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                tmp = tmp * i;
+            }
+            return tmp;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="st">first argument of the T</param>
+        /// <param name="F">second argument of the T</param>
+        /// <returns>returns the value of the T</returns>
+
+        public static double T(double st, double F)
+        {
+            double T = Math.Abs(st - F) / F * 1;
+            return T;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /// <summary>
+        /// Call the library method  Sk, S, st with ClassLibrary1.dll
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("   Завдання 1.\nЗнаходження кiлькостi повних тижнiв.\n");
+            Console.Write("Введiть кiлькiсть дiб: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кiлькость повних тижнiв: " + Class1.Sk(age));
+            Console.WriteLine("____________________________________________________________________________________________\n");
+
+
+            Console.WriteLine("   Завдання 2.\nЗнаходження суми.\n");
+            Console.Write("Введiть натуральне число n: ");
+            int an = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("S=" + Class1.S(an));
+            Console.WriteLine("____________________________________________________________________________________________\n");
+
+            /// <summary>
+            /// Call the library method Sum with Class1
+            /// </summary>
+            /// <param name = 'args'></param>
+            
+            Console.WriteLine("   Завдання 3.\nОбчислення суми.\n");
+            Console.WriteLine("Введiть значення eps:");
+            double eps = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть значення х:");
+            double z = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть кiлькiсть членiв ряду:");
+            int n = int.Parse(Console.ReadLine());
+
+            double st = ClassLibrary1.Class1.st(eps, n, z);
+            double F = ClassLibrary1.Class1.F(eps, n, z);
+            double t = ClassLibrary1.Class1.T(st, F);
+
+            Console.WriteLine("Сума ряду = {0},\nТочне значення функцiї для z = {1},\nПомилка обчислень = {2}", st, F, t);
+            Console.ReadKey();
+            Console.WriteLine("____________________________________________________________________________________________\n");
+        }
+    }
+}
